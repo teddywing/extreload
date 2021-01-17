19cf4f757250f92da5a4249c20781401c1750327
Teddy Wing <git@teddywing.com>
2021-01-17 18:32:24 +0100

    extreload: Send extension IDs to extension
    Send hard-coded extension IDs to the companion extension to tell it to
    reload them. Start an event loop to keep the native host running
    indefinitely.
    
    We'll then be able to send extension IDs from a command line client to
    the native host over a Unix domain socket IPC.

--- extreload/src/main.rs
+++ extreload/src/main.rs
@@ -1,3 +1,24 @@
+use chrome_native_messaging;
+use serde::Serialize;
+
+use std::io;
+
+#[derive(Serialize)]
+struct ExtensionIdMessage<'a> {
+    ids: &'a[&'a str]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ids = vec!["extension_id"];
+
+    chrome_native_messaging::send_message(
+        io::stdout(),
+        &ExtensionIdMessage { ids: &ids }
+    ).expect("TODO: handle error");
+
+    chrome_native_messaging::event_loop(|value| -> Result<(), &str> {
+        match value {
+            _ => Ok(()),
+        }
+    });
 }
