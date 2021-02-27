e7cb0b8256eefa8dfa07cfee2f4335bf6a414c72
Teddy Wing <git@teddywing.com>
2021-02-27 17:03:21 +0100

    macro: Add documentation

--- l/src/macro.lisp
+++ l/src/macro.lisp
@@ -1,9 +1,13 @@
 (in-package :extreload)
 
 (defmacro filter (predicate list-form)
+  "Returns a sequence that only includes elements of `list-form` that satisfy
+the test of `predicate`."
   `(remove-if-not ,predicate ,list-form))
 
 (defmacro with-websocket-connection ((client) &body body)
+  "Open a WebSocket connection on `client` and run `body` forms. The connection
+is automatically closed at the end of execution."
   `(progn
      (wsd:start-connection ,client)
      (unwind-protect (progn ,@body)
